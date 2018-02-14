b6dce8cd03a36446464c2bb4eb71a81dcd24dbfd
Mathieu Federak <mfederak@gmail.com>
2018-02-14 12:16:50 +0100

    added reviews routes nested in restaurants routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants, :except => [:update, :destroy]
+  resources :restaurants, :except => [:update, :destroy] do
+    resources :review, only: [:new, :create]
+  end
 end
