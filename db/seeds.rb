a80a7530d994778d6867d928b32fb8be2937381f
Mathieu Federak <mfederak@gmail.com>
2018-02-14 11:58:11 +0100

    added two models, restaurant and review

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'Belgian',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+  }
+  {
+    name:         'Arigato',
+    address:      'rue Saint-Anne',
+    category:     'japanese',
+  }
+  {
+    name:         'East Mama',
+    address:      'Ledru-Rollin',
+    category:     'italian',
+  }
+  {
+    name:         'Alicheur',
+    address:      'rue Oberkampf',
+    category:     'french',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
